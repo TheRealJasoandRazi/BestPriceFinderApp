6fcc500174af36021a7fbea07e5c81976bcdcdd2
TheRealRaziandJaso <Neno701@outlook.com>
2024-11-05 19:42:43 +1100

    Home page prototype

---
+++ lib/Home.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'ItemBar.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Expanded(
+            flex: 1, 
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Will need to make a widget for the search bar"),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 12.0),
+                    child: Text(
+                      "Popular Items:",
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                  )
+                ),
+                const Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ItemBar(image: "IMAGE", price: "2.00", location: "Earth", name: "Mars Bar"),
+                    ItemBar(image: "IMAGE", price: "1.50", location: "Mars", name: "Milky Way Bar"),
+                    ItemBar(image: "IMAGE", price: "200", location: "Melbourne", name: "Cheese"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 4, 
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 12.0),
+                    child: Text(
+                      "Recently Viewed:",
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                  )
+                ),
+                const Column(
+                  children: [
+                    ItemBar(image: "IMAGE", price: "2.00", location: "Earth", name: "Mars Bar"),
+                    ItemBar(image: "IMAGE", price: "1.50", location: "Mars", name: "Milky Way Bar"),
+                    ItemBar(image: "IMAGE", price: "200", location: "Melbourne", name: "Cheese"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/ItemBar.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class ItemBar extends StatelessWidget {
+  final String image;
+  final String price;
+  final String location;
+  final String name;
+
+  const ItemBar({
+    Key? key,
+    required this.image,
+    required this.price,
+    required this.location,
+    required this.name,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: FractionallySizedBox(
+        widthFactor: 0.8, // Takes up 80% of the screen width
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(8), // Adds rounded corners if desired
+          ),
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //Using Expanded instead of flexible causes the widget to try and expand verrtically since its in a column even if theres no room, causing an error, use flexible instead
+              Flexible( // IMAGE section
+                flex: 1,
+                fit: FlexFit.tight,
+                child: Container(
+                  child: Center(
+                    child: Text(
+                      image,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                )
+              ),
+              Flexible( // METADATA section
+                flex: 2,
+                fit: FlexFit.tight,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      name,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Text(
+                      "$price • $location",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+              Flexible( // BUTTON section
+                flex: 1,
+                fit: FlexFit.tight,
+                child: Container(
+                  child: Center(
+                    child: Text(
+                      "ADD TO LIST",
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  ),
+                )
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
